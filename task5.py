39dd288061f26b62f515f5d60872262110462871
Abhinesh77 <56187019+Abhinesh77@users.noreply.github.com>
2020-11-27 19:04:33 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,13 @@
+def cal():
+    a=int(input("enter two values:"));
+    b=int(input());
+    print(f"Addition of two numbers is {a+b}");
+    print(f"Subtraction of two numbers is {a-b}");
+    print(f"Division of two numbers is {a/b}");
+    print(f"Multiplication of two numbers is {a*b}");
+  
+cal();
+def covid(name,b_temp=98):
+    print(f"name:{name} \nbody temperature:{b_temp}");
+covid("a");
+covid("abhi",95);          
