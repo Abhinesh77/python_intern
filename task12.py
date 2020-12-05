c746bedb27321f28f78aa15f807090a0d6308aea
Abhinesh77 <56187019+Abhinesh77@users.noreply.github.com>
2020-12-05 15:51:57 +0530

    Update task12.py

--- task12.py
+++ task12.py
@@ -1,19 +1,26 @@
-import json
-#dictionary
-print(json.dumps({"Name":"john","doe":20}))
-#list
-print(json.dumps(["Amala","Dany"]))
-#string
-print(json.dumps(("Carolina","Matt")))
-#tuple
-print(json.dumps("Elena"))
-#integer
-print(json.dumps(80))
-#float
-print(json.dumps(25.1))
-#True
-print(json.dumps(True))
-#False
-print(json.dumps(False))
-#None
-print(json.dumps(None))
+import  json
+x={
+    "Name":"aaa",
+    "Age":20,
+    "city":"xyz",
+    "grade": None,
+    "result": True,
+    "marks1":{"bio":95,"CS":100},
+    "marks2":[90,98,99],
+    "avg":95.6
+}
+y=json.dumps(x)
+print(y)
+print(json.dumps((1,2,3)))
+print(json.dumps(list(range(9))))
+
+from pymongo import MongoClient
+myclient=MongoClient("mongodb://localhost:27017/") #making connection
+db=myclient["ABC"] #database
+Collection=db["data"]
+with open('D:\\Python Internship\\task12\\data.json') as f:
+    file_data=json.load(f)
+if isinstance(file_data,list):
+    Collection.insert_many(file_data)
+else:
+    Collection.insert_one(file_data)
